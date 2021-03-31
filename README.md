3bd06fe7fd2a874b010e8bf07dd6c765908366fc
Dedy Koeswara Ibrahim <dedy.koeswara.i@gmail.com>
2021-03-31 12:07:38 +0700

    Add To Git

---
+++ README.md
@@ -0,0 +1,14 @@
+# tugas2_2
+
+=======================================================
+
+Mobile Programming | Flutter
+
+NPM : 065118156
+
+Nama : Dedy Koeswara Ibrahim
+
+=======================================================
+
+## ScreenShot Output 
+<img width="300" height="auto" src="https://github.com/dedykoeswara/mobprog-tugas2.2/blob/eda72b9d4853b2e5de2bcc24268e197c26f3381c/screenshot/ss-1.jpg">
---
+++ lib/main.dart
@@ -0,0 +1,117 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                width: 150,
+                height: 150,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                    image: AssetImage('assets/images/dedy.jpg'),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                'Dedy Koeswara Ibrahim',
+                style: TextStyle(
+                    fontSize: 32,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white),
+              ),
+              Text(
+                'FMIPA | Ilmu Komputer',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18,
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(bottom: 25, top: 15),
+                width: MediaQuery.of(context).size.width * 0.4,
+                height: 1,
+                color: Colors.white,
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 24),
+                padding: EdgeInsets.symmetric(horizontal: 24, vertical: 14),
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(5),
+                  color: Colors.white,
+                ),
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.call,
+                      color: Colors.black,
+                      size: 30,
+                    ),
+                    SizedBox(
+                      width: 24,
+                    ),
+                    Text(
+                      '+62812-8185-9561',
+                      style: TextStyle(fontSize: 16, letterSpacing: 1),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 10),
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 24),
+                padding: EdgeInsets.symmetric(horizontal: 24, vertical: 14),
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(5),
+                  color: Colors.white,
+                ),
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.email,
+                      color: Colors.black,
+                      size: 30,
+                    ),
+                    SizedBox(
+                      width: 24,
+                    ),
+                    Text(
+                      'dedy.koeswara.i@gmail.com',
+                      style: TextStyle(fontSize: 16, letterSpacing: 1),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
